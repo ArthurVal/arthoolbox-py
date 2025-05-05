efa7e65e9bc8941e003d7d87357565f352bc4442
Arthur Valiente <valiente.arthur@gmail.com>
2025-05-05 13:34:53 +0200

    Add few lib stuff

---
+++ src/atb-py/math/statistics.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python3
+
+"""Module containing specifics statistics function like recursive computation.
+
+Functions list:
+- update_mean()        | Compute an updated mean
+- update_variance()    | Compute a new variance
+- update_sum_squares() | Compute a new sum of squares of differences from mean
+
+Classes list:
+- OnlineStatistics | Automatically compute the stats of a specific measurement
+"""
+from collections import namedtupled
+from threading import Condition
+
+
+def update_mean(new_data, old_mean, num_data):
+    """Compute a new mean recursively using the old mean and new measurement.
+
+    From the arithmetic mean computed using the n-1 measurements (M_n-1), we
+    compute the new mean (M_n) adding a new measurement (X_n) with the formula:
+    M_n = M_n-1 + (X_n - M_n-1)/n
+
+    See: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+    Parameters
+    ----------
+    new_data: int or decimal
+        The new measurement Xn
+    old_mean: int or decimal
+        The mean Mn-1 computed previously
+    num_data:
+        The number of data used to compute Mn
+
+    Returns
+    -------
+    float
+        The new mean Mn updated with Xn
+    """
+    return (old_mean + (new_data - old_mean) / num_data)
+
+
+def update_variance(new_data, old_variance, new_mean, old_mean, num_data):
+    """Compute a new variance recursively using the old and new mean.
+
+    From the previously computed variance V_n-1, the new and old mean M_n and
+    M_n-1 and the new measurement X_n, we compute an update value of the new
+    variance using the formula:
+    V_n = V_n-1 + (((X_n - M_n)*(X_n - M_n-1) - V_n-1) / n)
+
+    See: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+    Notes
+    -----
+    This formula may be unstable due to the floating point calculation issues.
+
+    Parameters
+    ----------
+    new_data: int or decimal
+        The new measurement X_n
+    old_variance: int or decimal
+        The variance V_n-1 computed previously
+    new_mean: int or decimal
+        The mean M_n computed on the current step n
+    old_mean: int or decimal
+        The mean M_n-1 computed previously
+    num_data:
+        The number of total data n
+
+    Returns
+    -------
+    float
+        The new variance M_n updated with X_n, V_n-1, M_n and M_n-1
+    """
+    return (
+        old_variance +
+        (
+            ((new_data - old_mean) * (new_data - new_mean) - old_variance)
+            / num_data
+        )
+    )
+
+
+def update_sum_squares(new_data, old_sum_squares, new_mean, old_mean):
+    """Compute the update of sum of squares of differences from the mean.
+
+    From the previously computed sum SUM_n-1, the new and old mean M_n and
+    M_n-1 and the new measurement X_n, we compute an update value of the new
+    sum of squares differences noted SUM_n using the formula:
+    SUM_n = SUM_n-1 + (X_n - M_n) * (X_n - M_n-1)
+
+    See: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+    This SUM can be use to compute the variance and sample variance:
+    Vn = SUM_n/n
+    Sn = SUM_n/(n+1)
+
+    This make the variance computation suffer less from floating point
+    computation instabilities.
+
+    Parameters
+    ----------
+    new_data: int or decimal
+        The new measurement X_n
+    old_sum_squares: int or decimal
+        The sum of squares SUM_n-1 computed previously
+    new_mean: int or decimal
+        The mean M_n computed on the current step n
+    old_mean: int or decimal
+        The mean M_n-1 computed previously
+
+    Returns
+    -------
+    float
+        The new sum of squares SUM_n updated with X_n, SUM_n-1, M_n and M_n-1
+    """
+    return (old_sum_squares + ((new_data - old_mean)*(new_data - new_mean)))
+
+
+class OnlineStatistics(object):
+    """Class use to update online the mean and variance of a measurement.
+
+    This class, when receiving a new measurement, automatically compute its
+    mean and variance/stdev recursively.
+
+    Ultimately, you can use the updated variable (threading.Condition) to sync
+    up your code when measurement has been done as it will notify you when new
+    measurement is used to update the mean/variance computation.
+
+    Warning
+    -------
+    Be aware that, when accessing the 'variance', 'sampled_variance' and
+    'stats' variables, those need to be computed based on the current internal
+    state of the OnlineStatistics object.
+    In order to avoid dead locks (because the 'updated' condition variable can
+    be initialized with a threading.Lock), we don't lock 'updated' for these
+    readings, but only when computing new values with 'measurement'.
+    If race conditions could appears between '.measurement = ' usage and
+    '.variance' (measurement updated in an other thread...), we highly advise
+    you to lock '.updated' BEFORE reading either 'variance', 'sampled_variance'
+    or 'stats'.
+
+    Attributs
+    ---------
+    updated: threading.Condition
+        Condition used to indicate that a measurement has been add, and
+        therefore we computed new values of mean/variance...
+    number_of_measurement: uint
+        The number of time this variable has been updated (n)
+    measurement: float
+        The new measurement at step = n (Xn)
+    mean: float
+        The mean Mn computed using recurrent equation.
+    variance: float
+        The variance Vn computed using recurrent equation.
+    sampled_variance: float
+        The sampled variance Sn computed using recurrent equation.
+    stats: collections.namedtuple(Stats, ['n', 'X', 'Mean', 'Var', 'SVar'])
+        The concatenated stats in one namedtuple
+    """
+
+    CurrentStat = namedtupled(
+        'Stats',
+        ['n', 'X', 'Mean', 'Var', 'SVar']
+    )
+
+    def __init__(self, condition_lock=None):
+        """Construct the Stat object with the provided  mutex."""
+        self.updated = Condition(condition_lock)
+        self.reset()
+
+    def __repr__(self):
+        """Make a string repr of the Stat object."""
+        return str(self.stats)
+
+    @property
+    def number_of_measurement(self):
+        """Return the current number of measurments done."""
+        return self.__n
+
+    @property
+    def measurement(self):
+        """Return the last measurment stored X."""
+        return self.__measurement
+
+    @measurement.setter
+    def measurement(self, new_measure):
+        """Update with a new measurment."""
+        with self.updated:
+            self.__n += 1
+            self.__measurement = new_measure
+            self.__mean, old_mean = (
+                update_mean(
+                    new_data=self.__measurement,
+                    old_mean=self.__mean,
+                    num_data=self.__n
+                ),
+                self.__mean
+            )
+            self.__sum_squares = update_sum_squares(
+                new_data=self.__measurement,
+                old_sum_squares=self.__sum_squares,
+                new_mean=self.__mean,
+                old_mean=old_mean
+            )
+            self.updated.notify_all()
+
+    @property
+    def mean(self):
+        """Get the mean. None if n <= 0."""
+        return self.__mean if self.__n > 0 else None
+
+    @property
+    def variance(self):
+        """Get the variance. None if n <= 0."""
+        return (self.__sum_squares / self.__n) if self.__n > 0 else None
+
+    @property
+    def sampled_variance(self):
+        """Get the sampled variance. None if n <= 1."""
+        return (self.__sum_squares / (self.__n - 1)) if self.__n > 1 else None
+
+    @property
+    def stats(self):
+        """Get all the statistics at once (n, X, Mean, Var, Sampled Var)."""
+        return OnlineStatistics.CurrentStat(
+            n=self.number_of_measurement,
+            X=self.measurement,
+            Mean=self.mean,
+            Var=self.variance,
+            SVar=self.sampled_variance,
+        )
+
+    def reset(self):
+        """Reset the Stat object."""
+        with self.updated:
+            self.__n = 0
+            self.__measurement = None
+            self.__mean = 0.
+            self.__sum_squares = 0.
---
+++ src/atb-py/time/sample.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+"""Module containing the sample decorator use to sample function calls.
+
+Good tutorial on decorator:
+https://realpython.com/primer-on-python-decorators/
+
+Decorators list:
+- sample | (opt) time_function | Compute mean period, stddev period, last_call
+  delta T etc..
+"""
+from functools import wraps
+from time import time
+
+from atb.math.statistics import OnlineStatistics
+
+
+def sample(_f=None, *, time_function=time.time):
+    """Compute timing parameters (mean/stddev period call) for a function.
+
+    This class is a decorator use to sample the decorated function. It can be
+    use to obtain the last period measured in sec (func.period.measurement)
+    between 2 function call, the mean, the variance and the sampled variance of
+    the period between function calls with func.period.mean,
+    func.period.variance and func.period.sampled_variance.
+
+    Additionnaly, you can synchronise your code with the func.period.updated
+    condition variable (threading.Condition) in order to be notified when new
+    period statistic is computed.
+
+    Attributs
+    ---------
+    last_call: float
+        The last __get_time() returned value use to compute measurements
+    period: arthoolbox.math.statistics.OnlineStatistics
+        OnlineStatistics object use to compute mean and variance online
+
+    Parameters
+    ----------
+    _f: Callable obj
+        The function to wrap around the sampling
+    time_function: Callable obj
+        Function use to measure time point (Default time.time)
+    """
+
+    def decorator_sample(f):
+        """Decorate and create the wrapper of f."""
+
+        @wraps(f)
+        def wrapper_sample(*args, **kwargs):
+            """Wrap f and perform statistics before calling it."""
+            # Function call -> Time measurements
+            now = time_function()
+
+            if wrapper_sample.last_call is not None:
+                # Register the new measurement
+                wrapper_sample.period.measurement = \
+                    now - wrapper_sample.last_call
+
+            wrapper_sample.last_call = now
+
+            return f(*args, **kwargs)
+
+        # Initialize the wrapper function with attribut (function are objects)
+        wrapper_sample.last_call = None
+        wrapper_sample.period = OnlineStatistics()
+
+        return wrapper_sample
+
+    if _f is None:
+        # sample called without unamed arguments -> First argument is the
+        # function:
+        #
+        # function = sample(function)
+        #
+        # OR
+        #
+        # @sample
+        # def function():
+        #   ...
+        return decorator_sample
+
+    else:
+        # sample called with time_function:
+        #
+        # function = sample(time_function = toto)(function)
+        #
+        # OR
+        #
+        # @sample(time_function = toto)
+        # def function():
+        #   ...
+        return decorator_sample(_f)
