1d8c3b181024d2eaa4c88653ac9b97c0976f59cb
ArthurVal <valiente.arthur@gmail.com>
2025-12-10 09:59:16 +0100

    Refactor functional/args (use singledispatch)

--- src/arthoolbox/functional/args.py
+++ src/arthoolbox/functional/args.py
@@ -5,97 +5,199 @@
 from dataclasses import (
     dataclass,
     KW_ONLY,
-    MISSING,
+)
+from functools import (
+    singledispatch,
 )
 
 from collections.abc import (
     Callable,
+    Iterable,
 )
 from typing import (
     Any,
     Text,
+    Tuple,
     Union,
     ParamSpec,
+    TypeVar,
 )
 
 
 P = ParamSpec("P")
 
+T = TypeVar("T")
+
+
+class _NotSet:
+    pass
+
+
+NotSet = _NotSet()
+
 
 @dataclass(frozen=True)
-class AtIdx:
-    """TODO."""
+class ArgAt[T]:
+    """Select an argument from a function argument list."""
 
-    i: Union[int | slice]
+    sel: Union[int, slice, Text]
     _: KW_ONLY
-    default: Any = MISSING
+    default: T = NotSet
 
 
-@dataclass(frozen=True)
-class WithKey:
-    """TODO."""
+@singledispatch
+def ForwardArg(sel, *args, **kwargs):
+    """Create a functor that forward an arg by index/slice/key.
 
-    key: Text
-    _: KW_ONLY
-    default: Any = MISSING
+    To select the arg(s) to forward, you can either give:
+    - A raw int index 'i': Forward args[i]
+    - A raw str key 'key': Forward kwargs[key]
+    - A slice (i.e. slice(0, 2, 3)): Forward args[slice] (a tuple)
 
+    Each sel type can also be associated to a default value by using
+    'default=V' keyword, when calling ForwardArg, in order to forward a default
+    value hwne the given selector is not contained within the arg list (i.e.
+    out of bound index access, etc...).
 
-def ForwardArg(arg: Union[int, AtIdx, Text, WithKey]) -> Callable[P, Any]:
-    """Create a functor that forward a single arg, either by index of key.
+    Additionally, you can use ArgAt(sel, *, default=V) to create a single arg
+    selection.
+
+    Also, the selection can be any Iterable that will be used to forward
+    multiple arguments, accodinging to the list of select arguments given.
+
 
     Parameters
     ----------
-    arg: Union[int, AtIdx, Text, WithKey]
-      If an interger is given, forwards args[i]. Otherwise (a key str), forward
-      kwargs[key].
-
+    sel: Union[int, slice, Text, ArgAt, Iterable]
+      The index/slice/key within the input args to forward
 
     Returns
     -------
     Callable[P, Any]
-      The functor that filter args
+      The functor that forward args by index, slice or key
+
     """
-    if isinstance(arg, (int, slice)):
-        return ForwardArg(AtIdx(arg))
-    elif isinstance(arg, Text):
-        return ForwardArg(WithKey(arg))
-    elif isinstance(arg, AtIdx):
-        if isinstance(arg.default, MISSING):
+    raise TypeError(f"Unknown arg selection {sel!r} (type: {type(sel)})")
 
-            def __impl(*args: P.args, **kwargs: P.kwargs) -> Any:
-                return args[arg.i]
 
-            __impl.__doc__ = f"Forward arg[{arg.i}]"
+@ForwardArg.register
+def _(
+    arg: ArgAt,
+    *,
+    default: T = NotSet,
+) -> Union[
+    Callable[P, Any],
+    Callable[P, Tuple[Any, ...]],
+    Callable[P, Union[Any, T]],
+    Callable[P, Union[Tuple[Any, ...], T]],
+]:
+    return ForwardArg(
+        arg.sel,
+        default=arg.default if arg.default != NotSet else default,
+    )
 
-        else:
 
-            def __impl(*args: P.args, **kwargs: P.kwargs) -> Any:
-                return args[arg.i] if len(args) < arg.i else arg.default
+@ForwardArg.register
+def _(
+    i: int,
+    *,
+    default: T = NotSet,
+) -> Union[
+    Callable[P, Any],
+    Callable[P, Union[Any, T]],
+]:
+    if default == NotSet:
 
-            __impl.__doc__ = (
-                f"Forward arg[{arg.i}] (default to: {arg.default!r})"
-            )
+        def __impl(*args: P.args, **kwargs: P.kwargs) -> Any:
+            return args[i]
 
-    elif isinstance(arg, WithKey):
-        if isinstance(arg.default, MISSING):
-
-            def __impl(*args: P.args, **kwargs: P.kwargs) -> Any:
-                return kwargs[arg.key]
-
-            __impl.__doc__ = f"Forward kwarg[{arg.key!r}]"
-
-        else:
-
-            def __impl(*args: P.args, **kwargs: P.kwargs) -> Any:
-                return kwargs.get(arg.key, default=arg.default)
-
-            __impl.__doc__ = (
-                f"Forward kwarg[{arg.key!r}] (default to: {arg.default!r})"
-            )
+        __impl.__doc__ = f"Forward arg[{i}]"
 
     else:
-        raise ValueError(
-            f"ID should be either an int or a str (got '{type(arg)}')"
+
+        def __impl(*args: P.args, **kwargs: P.kwargs) -> Union[Any, T]:
+            return args[i] if i < len(args) else default
+
+        __impl.__doc__ = f"Forward arg[{i}] (default to {default})"
+
+    return __impl
+
+
+@ForwardArg.register
+def _(
+    s: slice,
+    *,
+    default: T = NotSet,
+) -> Union[
+    Callable[P, Tuple[Any, ...]],
+    Callable[P, Union[Tuple[Any, ...], T]],
+]:
+    if default == NotSet:
+
+        def __impl(*args: P.args, **kwargs: P.kwargs) -> Tuple[Any, ...]:
+            return args[s]
+
+        __impl.__doc__ = f"Forward arg[{s}]"
+
+    else:
+
+        def __impl(
+            *args: P.args, **kwargs: P.kwargs
+        ) -> Union[Tuple[Any, ...], T]:
+            return args[s] if s < len(args) else default
+
+        __impl.__doc__ = f"Forward arg[{s}] (default to {default})"
+
+    return __impl
+
+
+@ForwardArg.register
+def _(
+    key: Text,
+    *,
+    default: T = NotSet,
+) -> Union[
+    Callable[P, Any],
+    Callable[P, Union[Any, T]],
+]:
+    if default == NotSet:
+
+        def __impl(*args: P.args, **kwargs: P.kwargs) -> Any:
+            return kwargs[key]
+
+        __impl.__doc__ = f"Forward kwarg[{key!r}]"
+
+    else:
+
+        def __impl(*args: P.args, **kwargs: P.kwargs) -> Union[Any, T]:
+            return kwargs.get(key, default)
+
+        __impl.__doc__ = f"Forward kwarg[{key!r}] (default to {default})"
+
+    return __impl
+
+
+@ForwardArg.register(Iterable)
+def _(
+    sels: Iterable[Any],
+    *,
+    default: T = NotSet,
+) -> Union[Callable[P, Tuple[Any, ...]],]:
+    getters = [ForwardArg(a, default=default) for a in sels]
+
+    def __impl(*args: P.args, **kwargs: P.kwargs) -> Tuple[Any, ...]:
+        return tuple(get(*args, **kwargs) for get in getters)
+
+    __impl.__doc__ = "Forward: {args}".format(
+        args=", ".join(
+            (
+                "({i}) {what}".format(
+                    i=i,
+                    what=get_arg.__doc__.removeprefix("Forward "),
+                )
+                for i, get_arg in enumerate(getters)
+            )
         )
+    )
 
     return __impl
