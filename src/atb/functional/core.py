946695f2670b7f663800fefdb177e26a4b24c4ba
ArthurVal <valiente.arthur@gmail.com>
2025-12-11 14:18:44 +0100

    Remove stringify stuff from core

--- src/atb/functional/core.py
+++ src/atb/functional/core.py
@@ -4,8 +4,6 @@
 
 from functools import (
     wraps,
-    update_wrapper,
-    partial,
 )
 
 from collections.abc import (
@@ -14,7 +12,6 @@ from collections.abc import (
 from typing import (
     Any,
     Text,
-    Union,
     ParamSpec,
     TypeVar,
 )
@@ -92,65 +89,3 @@ def Raises(err: T) -> Callable[P, None]:
     __impl.__doc__ = f"Raises {err!r}"
 
     return __impl
-
-
-class StringifyWrapper:
-    """Wrapper that give the possibility to implement a better/custom str().
-
-    May be usefull when declaring runtime functions/lambdas and give them a
-    better description that changes based on the context.
-    """
-
-    def __init__(
-        self,
-        f: Callable[P, R],
-        to_str: Union[Text, Callable[[Callable[P, R]], Text]],
-    ) -> None:
-        """Create the wrapper adding a custom __str__.
-
-        Parameters
-        ----------
-        f: Callable[P, R]
-          Wrapped function being decorated.
-        to_str: Union[Text, Callable[[Callable[P, R]],Text]]
-          Either a raw string, that will always be returned whenever calling
-          `str()`, OR, a callable that handle dynamic `str()` values, called
-          with `f` eveytime in order to create the description dynamically.
-        """
-        update_wrapper(self, f)
-        self.__to_str = Returns(to_str) if isinstance(to_str, Text) else to_str
-
-    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
-        """Call the underlying wrapped callable."""
-        return self.__wrapped__(*args, **kwargs)
-
-    def __str__(self) -> Text:
-        """Stringify the underlying wrapped callable."""
-        return self.__to_str(self.__wrapped__)
-
-    def __repr__(self) -> Text:
-        """Repr of the StringifyWrapper."""
-        return "StringifyWrapper(f={f}, to_str={to_str})".format(
-            f=repr(self.__wrapped__),
-            to_str=Brief(self.__to_str),
-        )
-
-
-def stringify(
-    s: Union[Text, Callable[[Callable[P, R]], Text]],
-) -> StringifyWrapper:
-    """Decorate a function to add a custom __str__() function.
-
-    Parameters
-    ----------
-    s: Union[Text, Callable[[Callable[P, R]],Text]]
-      Either a raw string, that will always be returned whenever calling
-      `str()`, OR, a function that handle dynamic `str()` values, called
-      with the function eveytime in order to create the description dynamically
-
-    Returns
-    -------
-    StringifyWrapper
-      A wrapper use to decorate any callable/function.
-    """
-    return partial(StringifyWrapper, to_str=s)
